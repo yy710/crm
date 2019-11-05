e714282fe13da87659f4fa3c5444bb0b56b4e4da
Yu Chunjian <yuchunjian@Yus-MacBook-Pro.local>
2019-11-05 14:32:57 +0800

    ent

---
+++ public/list-referred.vue
@@ -0,0 +1,21 @@
+<template>
+  <van-panel title="姓名：夏侯渊" desc="电话：13000000000" status="新客户">
+    <div>此处显示用户画像信息</div>
+    <div slot="footer">
+      <van-button round disabled type="warning" size="small">三天后回访</van-button>
+    </div>
+  </van-panel>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ public/new-referred.vue
@@ -0,0 +1,20 @@
+<template>
+  <van-cell-group>
+    <van-field v-model="username" required label="客户姓名" left-icon="contact" placeholder="请输入客户姓名"></van-field>
+    <van-field v-model="phone" required label="手机号" placeholder="请输入手机号" left-icon="phone-o"></van-field>
+  </van-cell-group>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      username: "world",
+      phone: null
+    };
+  }
+};
+</script>
+
+<style>
+</style>
